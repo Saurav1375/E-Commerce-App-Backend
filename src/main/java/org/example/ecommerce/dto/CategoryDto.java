f77b50a37120d498e7919470c9cf033e4610a2c5
Saurav1375 <sauravgupta1375@gmail.com>
2025-03-19 18:27:32 +0530

    added getAllCategories Method

---
+++ src/main/java/org/example/ecommerce/dto/CategoryDto.java
@@ -0,0 +1,11 @@
+package org.example.ecommerce.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CategoryDto {
+    private int id;
+    private String name;
+}
